dff0fab4e28433cc200f8fc697040cc249ef0471
temporraryaccounthasbeensuspended <wweziza@icloud.com>
2024-10-06 02:58:59 +0700

    example database

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE TABLE accounts (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    admin TINYINT(1) DEFAULT 0,
+    balance DECIMAL(10,2) DEFAULT 0.00,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(24) NOT NULL,
+    product_type VARCHAR(10) NOT NULL,
+    product_amount INT NOT NULL,
+    product_bonus INT NOT NULL,
+    product_price INT NOT NULL
+);
